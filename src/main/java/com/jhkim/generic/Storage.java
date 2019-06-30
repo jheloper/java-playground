2279521c864df62d895d3e36b2c30910e81f8ba1
Joonghyeon Kim <jheloper9102@gmail.com>
2019-06-30 15:44:38 +0900

    제네릭 예제에서 사용할 Storage 클래스 추가

---
+++ src/main/java/com/jhkim/generic/Storage.java
@@ -0,0 +1,16 @@
+package com.jhkim.generic;
+
+public class Storage {
+
+    private Object storedData;
+
+
+    public Object getStoredData() {
+        return storedData;
+    }
+
+
+    public void setStoredData(Object storedData) {
+        this.storedData = storedData;
+    }
+}

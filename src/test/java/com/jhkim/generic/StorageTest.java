a78de0227d89bdb0e334543358e78abc7fa7fdbe
Joonghyeon Kim <jheloper9102@gmail.com>
2019-06-30 15:51:11 +0900

    Storage 클래스의 data 필드에 여러 타입의 데이터를 넣고 가져오는 테스트 코드 추가

--- src/test/java/com/jhkim/generic/StorageTest.java
+++ src/test/java/com/jhkim/generic/StorageTest.java
@@ -13,7 +13,39 @@ public class StorageTest {
         Storage storage = new Storage();
         storage.setStoredData(data);
 
+        // String 타입으로 캐스팅하는 코드를 넣지 않으면 컴파일 에러 발생.
         String storedData = (String) storage.getStoredData();
         Assert.assertEquals(data, storedData);
+    }
+
+
+    @Test
+    public void setAndGetStoredDataWithMultipleTypes() {
+
+        final Integer data = 1;
+        // final String data = "this is data."
+        // final Double data = 3.14;
+
+        Storage storage = new Storage();
+        storage.setStoredData(data);
+
+        if(storage.getStoredData() instanceof String) {
+
+            final String storedData = (String) storage.getStoredData();
+            Assert.assertEquals(data, storedData);
+
+        } else if(storage.getStoredData() instanceof Integer) {
+
+            final Integer storedData = (Integer) storage.getStoredData();
+            Assert.assertEquals(data, storedData);
+
+        } else if(storage.getStoredData() instanceof Double) {
+
+            final Double storedData = (Double) storage.getStoredData();
+            Assert.assertEquals(data, storedData);
+
+        } else {
+            Assert.fail("unsupported type.");
+        }
     }
 }

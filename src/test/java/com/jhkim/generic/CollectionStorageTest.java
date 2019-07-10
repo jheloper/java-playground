5c5440af7a6ee32d484fdac37060d6a72ea71f0b
Joonghyeon Kim <jheloper9102@gmail.com>
2019-07-10 23:28:30 +0900

    List<Number> 대신 List<Integer>를 사용할 경우에 대한 주석 추가

--- src/test/java/com/jhkim/generic/CollectionStorageTest.java
+++ src/test/java/com/jhkim/generic/CollectionStorageTest.java
@@ -11,6 +11,8 @@ public class CollectionStorageTest {
     public void setAndGetCollection() {
 
         final List<Number> collection = new ArrayList<>();
+        // Integer 클래스는 Number 클래스를 상속했지만 파라미터로 할당하면 컴파일 에러 발생
+        // final List<Integer> collection = new ArrayList<>();
         collection.add(1);
         collection.add(2);
 

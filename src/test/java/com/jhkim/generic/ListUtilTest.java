c386755003c812308099f6e71387461bb692d708
Joonghyeon Kim <jheloper9102@gmail.com>
2019-07-13 20:03:25 +0900

    List<Number> 대신 List<Integer>를 파라미터로 넘길 경우에 대한 주석 추가

--- src/test/java/com/jhkim/generic/ListUtilTest.java
+++ src/test/java/com/jhkim/generic/ListUtilTest.java
@@ -9,7 +9,11 @@ public class ListUtilTest {
     @Test
     public void printElements() {
 
+        // error: incompatible types: List<Integer> cannot be converted to List<Number>
+        // List<Number>와 List<Integer>는 무관
+        // final List<Integer> list = new ArrayList<>();
         final List<Number> list = new ArrayList<>();
+
         list.add(1);
         list.add(2);
         list.add(3);

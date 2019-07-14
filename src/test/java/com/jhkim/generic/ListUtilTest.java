c3443fcc1646940ea7a4b456ae4d8c5be9ac034e
Joonghyeon Kim <jheloper9102@gmail.com>
2019-07-14 16:28:15 +0900

    printElements 메서드 파라미터에 List<Object>를 넘기도록 테스트 코드 수정

--- src/test/java/com/jhkim/generic/ListUtilTest.java
+++ src/test/java/com/jhkim/generic/ListUtilTest.java
@@ -11,8 +11,11 @@ public class ListUtilTest {
 
         // error: incompatible types: List<Integer> cannot be converted to List<Number>
         // List<Number>와 List<Integer>는 무관
-        final List<Integer> list = new ArrayList<>();
+        // final List<Integer> list = new ArrayList<>();
         // final List<Number> list = new ArrayList<>();
+
+        // printElements의 타입 파라미터를 와일드카드 타입으로 지정했기 때문에 파라미터로 넘길 수 있음
+        final List<Object> list = new ArrayList<>();
 
         list.add(1);
         list.add(2);

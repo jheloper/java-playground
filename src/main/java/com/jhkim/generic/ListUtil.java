e14e008be940564f406d9df1dd5b54fba5641e19
Joonghyeon Kim <jheloper9102@gmail.com>
2019-07-14 20:00:34 +0900

    printElements 메서드를 와일드카드 타입 사용하는 경우, 한정적 와일드카입을 사용하는 경우 2가지를 포함한 총 3가지 경우로 분리, 이에 따라 테스트 코드 수정

--- src/main/java/com/jhkim/generic/ListUtil.java
+++ src/main/java/com/jhkim/generic/ListUtil.java
@@ -4,7 +4,23 @@ import java.util.List;
 
 public class ListUtil {
 
-    public static void printElements(List<? extends Number> list) {
+    public static void printElements(List<?> list) {
+
+        for(int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+
+    public static void printNumberExtendElements(List<? extends Number> list) {
+
+        for(int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+
+    public static void printIntegerSuperElements(List<? super Integer> list) {
 
         for(int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
--- src/test/java/com/jhkim/generic/ListUtilTest.java
+++ src/test/java/com/jhkim/generic/ListUtilTest.java
@@ -11,17 +11,51 @@ public class ListUtilTest {
 
         // error: incompatible types: List<Integer> cannot be converted to List<Number>
         // List<Number>와 List<Integer>는 무관
-        final List<Integer> list = new ArrayList<>();
+        // final List<Integer> list = new ArrayList<>();
         // final List<Number> list = new ArrayList<>();
 
         // printElements의 타입 파라미터를 와일드카드 타입으로 지정했기 때문에 파라미터로 넘길 수 있음
-        // 한정적 와일드카드 타입으로 변경하면 아래 List<Object>는 컴파일 오류 발생
-        // final List<Object> list = new ArrayList<>();
+        // 다만, 한정적 와일드카드 타입으로 변경하면 아래 List<Object>는 컴파일 오류 발생
+        final List<Object> list = new ArrayList<>();
 
         list.add(1);
         list.add(2);
         list.add(3);
 
-        ListUtil.<Number>printElements(list);
+        ListUtil.printElements(list);
+    }
+
+
+    @Test
+    public void printNumberExtendElements() {
+
+        // error: incompatible types: List<Object> cannot be converted to List<? extends Number>
+        // Object는 Number 또는 Number의 하위 클래스가 아니기 때문에 컴파일 오류 발생
+        // final List<Object> list = new ArrayList<>();
+
+        final List<Long> list = new ArrayList<>();
+
+        list.add(1L);
+        list.add(2L);
+        list.add(3L);
+
+        ListUtil.printNumberExtendElements(list);
+    }
+
+
+    @Test
+    public void printIntegerSuperElements() {
+
+        // error: incompatible types: List<Long> cannot be converted to List<? super Integer>
+        // Long은 Integer 또는 Integer의 상위 클래스가 아니기 때문에 컴파일 오류 발생
+        // final List<Long> list = new ArrayList<>();
+
+        final List<Number> list = new ArrayList<>();
+
+        list.add(1L);
+        list.add(2L);
+        list.add(3L);
+
+        ListUtil.printIntegerSuperElements(list);
     }
 }

f3644bcf3b450be27d50983028ae590fcb20ceb9
Joonghyeon Kim <jheloper9102@gmail.com>
2019-07-14 19:48:44 +0900

    printElements 메서드 파라미터에 한정적 와일드카드 타입을 적용하도록 수정 및 테스트 코드 수정

--- src/main/java/com/jhkim/generic/ListUtil.java
+++ src/main/java/com/jhkim/generic/ListUtil.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class ListUtil {
 
-    public static void printElements(List<?> list) {
+    public static void printElements(List<? extends Number> list) {
 
         for(int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
